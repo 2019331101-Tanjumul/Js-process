a0e4fa24fd7cd9ce265db7649042d4060f38969f
2019331101-Tanjumul <crackerroot4@gmail.com>
2024-01-02 04:36:08 +0600

    late commit but first commit for js

---
+++ 2_progressday.js
@@ -0,0 +1,7 @@
+//alert(3+3); //we'r using node js not browser 
+
+console.log(3+3); 
+console.log("Tanju"); 
+
+//code readability shuld be high 
+//use ecma script to be informed for js
---
+++ variables.js
@@ -0,0 +1,18 @@
+const accountID =123445
+let accountEmail = "Tanju@gmai.com"
+var accountPassword = "123455"
+accountCity = "Gaibandha"
+//accountID = 2 // not allowed
+
+console.log(accountID);
+
+/* 
+not using this to use this 
+
+*/ 
+//alert statement usage ! 
+
+//alert(3+3); //we'r using node js not browser 
+
+console.log(3+3); 
+console.log("Tanju"); 
