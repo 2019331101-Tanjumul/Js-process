13e3f40876e8ddf18b99a0a256f7000cba5966dc
2019331101-Tanjumul <crackerroot4@gmail.com>
2024-01-03 22:51:23 +0600

    operations

---
+++ 4_progressday.js
@@ -0,0 +1,9 @@
+let value = 3; 
+let negValue = -value; 
+//console.log(negValue); 
+//basic operations  
+let str1 = "Hello"
+let str2 = "Tanju"
+let str = str1 +" "+ str2; 
+
+console.log(" "+ str); 
