855b5d7a220e98ce6552fbeb3a23efa7e7b65746
2019331101-Tanjumul <crackerroot4@gmail.com>
2024-01-05 14:49:54 +0600

    additional codes

--- 4_progressday.js
+++ 4_progressday.js
@@ -6,4 +6,13 @@ let str1 = "Hello"
 let str2 = "Tanju"
 let str = str1 +" "+ str2; 
 
-console.log(" "+ str); 
+//console.log(" "+ str); 
+//console.log(true)
+
+let num1, num2 , num3 
+num1 = num2 = num3 = 2+2; 
+console.log(num2) 
+let gamecounter = 100; 
+++gamecounter; 
+console.log(gamecounter) /*prefix and postfix js mdn checkout
+for incrimental and decremental process*/
