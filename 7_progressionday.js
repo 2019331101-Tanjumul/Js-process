8960f01aa200f6469a90fd375bcc1363d6efed96
2019331101-Tanjumul <crackerroot4@gmail.com>
2024-01-10 02:58:22 +0600

    the object and the addressing of a character in the string

--- 7_progressionday.js
+++ 7_progressionday.js
@@ -9,4 +9,16 @@ we use backticks for new declaration of a string or a variable thing on the outp
 
 let Countstringfor = new String('Tanjumul-Alom')
 
-console.log(Countstringfor) 
+//console.log(Countstringfor) 
+
+console.log(Countstringfor[1]); 
+console.log(Countstringfor.__proto__); 
+/* the output from here shows the 
+a 
+{}
+here the 1th index is the a of Tanjumul-Alom 
+that's why it is "a" and the 
+{} is also defining as the empty object but
+it is not the empty in this case 
+ */
+
