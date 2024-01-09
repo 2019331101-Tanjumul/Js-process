31b980c77c95c8a173c7bb49544d2e7d728e395e
2019331101-Tanjumul <crackerroot4@gmail.com>
2024-01-10 01:17:46 +0600

    About string untill string = new string()

--- 7_progressionday.js
+++ 7_progressionday.js
@@ -1,1 +1,12 @@
 /* string on this file tho  */
+const name = "Tanjumul"
+let ageofName = 24 
+
+console.log( `Here the name of the consumer of this service is ${name} and the age of this person is ${ageofName}` ) /* except of using the double quotations ("") , 
+we use backticks for new declaration of a string or a variable thing on the output */
+
+//another way to declare the string is going to be  
+
+let Countstringfor = new String('Tanjumul-Alom')
+
+console.log(Countstringfor) 
