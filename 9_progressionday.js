922e4219f7ffe828ae88af193a902ebe98f0feff
2019331101 <crackerroot4@gmail.com>
2024-01-15 04:58:50 +0600

    commit for the process of learning js and journey of this code

---
+++ 9_progressionday.js
@@ -0,0 +1,5 @@
+let age = 40;
+const crip =
+  "i will learn the full js and the backend vides then docker , kuberneties , rancher and the aws cloud for my personal satisfaction";
+
+// let conn = ` This is also a practice of my previous work of the java script which is long about ${age} and ${crip}`
