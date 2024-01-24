9592494991a27261cab7587134dfecfa3bcd56b5
2019331101 <crackerroot4@gmail.com>
2024-01-24 11:29:33 +0600

    changes on Primitive

--- 6_progressionday.js
+++ 6_progressionday.js
@@ -1,48 +1,42 @@
 //premitive and non premititive of types of data
 
-//call by value and call by referance 
+//call by value and call by referance
 /* actually it is the way that we use to 
 store the values and get those values from the 
 store */
 
 //primitive
-// 7 catagories : call by value , copy change : string , null , boolean , number , undefined , symble 
-//Bigint 
+// 7 catagories : call by value , copy change : string , null , boolean , number , undefined , symble
+//Bigint
 
-const score = 200 
-const scorevalue = 100.2 
-const isLoggedin = false 
-const outsideTemp = null 
-let userEmail = undefined ; 
-// we can alse use let userEmail;  for undefined 
-const id = Symbol('123')
-const Anotherid = Symbol('123')
+const score = 200;
+const scorevalue = 100.2;
+const isLoggedin = false;
+const outsideTemp = null;
+let userEmail = undefined;
+// we can alse use let userEmail;  for undefined
+const id = Symbol("123");
+const Anotherid = Symbol("123");
 
-console.log(id === Anotherid); 
+console.log(id === Anotherid);
 
-const bigNumber = 1333131113144155n
+const bigNumber = 1333131113144155n;
 //by adding the n in the last of an big integer it will become a BigInt
 
 const heroes = ["Tanju", "Tanjumul", "Alom"];
 
 let myobj = {
-    name : "Tanjumull",
-    age: 23 
+  name: "Tanjumull",
+  age: 23,
+};
 
-}
-
-// const myFunction (){ 
+// const myFunction (){
 // console.log("Hello World !!")
 
 // }
 
-//now see the all type of the values or thed 
+//now see the all type of the values or thed
 //datadypes of the values
-
-
-
-
-
 
 /* Referance type : (non primitive) 
 
@@ -50,25 +44,24 @@ array , objects , Functions
 
 */
 
-
 //++++++++++++++++++++++++++++++++++++++++++++++++=+
 
 //stack(primitive), Heap ( non-primitive)
 
-let myYoutubename = "TanjumulGaming"
+let myYoutubename = "TanjumulGaming";
 
-let anotherYoutubeName = myYoutubename 
-anotherYoutubeName = "TanjuYogg"
+let anotherYoutubeName = myYoutubename;
+anotherYoutubeName = "TanjuYogg";
 
-console.log(myYoutubename)
-console.log(anotherYoutubeName)
-let userOne = { 
-    name: "Tanjumul", 
-    email : "balbla@gmail.com",
-    bkash: 017 ,
-}
+console.log(myYoutubename);
+console.log(anotherYoutubeName);
+let userOne = {
+  name: "Tanjumul",
+  email: "balbla@gmail.com",
+  bkash: 017,
+};
 
-let userTwo = userOne 
-userTwo.email = "tanju@google.com"
-console.log(userOne.email)
-console.log(userTwo.email)
+let userTwo = userOne;
+userTwo.email = "tanju@google.com";
+console.log(userOne.email);
+console.log(userTwo.email);
